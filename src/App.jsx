91248c1d9e90ed2042101b87141efe704a84c88b
isakibul <isakibul108@gmail.com>
2024-07-29 09:48:42 +0600

    spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MultiStepForm from './form/MultiStepForm';
+import FormField from './form/MultiStepForm';
 import ThankYouPage from './form/ThankYouPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MultiStepForm />} />
+        <Route path="/" element={<FormField />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
       </Routes>
     </Router>
--- src/form/MultiStepForm.jsx
+++ src/form/MultiStepForm.jsx
@@ -5,96 +5,96 @@ import SecondStep from './SecondStep';
 import ThirdStep from './ThirdStep';
 import CustomModal from './CustomModal';
 import axios from 'axios';
-import { ClipLoader } from 'react-spinners';
+import { HashLoader } from 'react-spinners';
 
 const MultiStepForm = () => {
-  const [step, setStep] = useState(1);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [formData, setFormData] = useState({
-    email: '',
-    firstName: '',
-    lastName: '',
-    country: '',
-    mobileNumber: '',
-    province: '',
-    city: '',
-    postcode: '',
-    involvement: '',
-    specialties: [],
-    referral: ''
-  });
-  const [isLoading, setIsLoading] = useState(false);
+    const [step, setStep] = useState(1);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+        country: '',
+        mobileNumber: '',
+        province: '',
+        city: '',
+        postcode: '',
+        involvement: '',
+        specialties: [],
+        referral: ''
+    });
+    const [isLoading, setIsLoading] = useState(false);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (modalIsOpen) {
-      document.body.classList.add('no-scroll');
-    } else {
-      document.body.classList.remove('no-scroll');
-    }
-  }, [modalIsOpen]);
+    useEffect(() => {
+        if (modalIsOpen) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }, [modalIsOpen]);
 
-  const nextStep = (values) => {
-    setFormData({ ...formData, ...values });
-    setStep(step + 1);
-  };
+    const nextStep = (values) => {
+        setFormData({ ...formData, ...values });
+        setStep(step + 1);
+    };
 
-  const prevStep = () => {
-    setStep(step - 1);
-  };
+    const prevStep = () => {
+        setStep(step - 1);
+    };
 
-  const handleSubmit = (values) => {
-    setFormData({ ...formData, ...values });
-    setIsLoading(true);
-    console.log('Submitting form data:', { ...formData, ...values });
+    const handleSubmit = (values) => {
+        setFormData({ ...formData, ...values });
+        setIsLoading(true);
+        console.log('Submitting form data:', { ...formData, ...values });
 
-    axios.post('https://muslimintech-server.onrender.com/api/register', { ...formData, ...values })
-      .then(response => {
-        console.log('Response data:', response.data);
-        navigate('/thank-you');
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-        setIsLoading(false);
-      });
-  };
+        axios.post('https://muslimintech-server.onrender.com/api/register', { ...formData, ...values })
+            .then(response => {
+                console.log('Response data:', response.data);
+                navigate('/thank-you');
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+                setIsLoading(false);
+            });
+    };
 
-  const handleCountryChange = (values) => {
-    if (values.country === "Other") {
-      setModalIsOpen(true);
-    } else {
-      nextStep(values);
-    }
-  };
+    const handleCountryChange = (values) => {
+        if (values.country === "Other") {
+            setModalIsOpen(true);
+        } else {
+            nextStep(values);
+        }
+    };
 
-  const renderStep = () => {
-    switch (step) {
-      case 1:
-        return <FirstStep nextStep={handleCountryChange} formData={formData} />;
-      case 2:
-        return <SecondStep nextStep={nextStep} prevStep={prevStep} formData={formData} />;
-      case 3:
-        return <ThirdStep handleSubmit={handleSubmit} prevStep={prevStep} formData={formData} />;
-      default:
-        return null;
-    }
-  };
+    const renderStep = () => {
+        switch (step) {
+            case 1:
+                return <FirstStep nextStep={handleCountryChange} formData={formData} />;
+            case 2:
+                return <SecondStep nextStep={nextStep} prevStep={prevStep} formData={formData} />;
+            case 3:
+                return <ThirdStep handleSubmit={handleSubmit} prevStep={prevStep} formData={formData} />;
+            default:
+                return null;
+        }
+    };
 
-  return (
-    <div>
-      {isLoading ? (
-        <div className="spinner-container">
-          <ClipLoader size={50} color={"#123abc"} loading={isLoading} />
+    return (
+        <div>
+            {isLoading ? (
+                <div className="spinner-container">
+                    <HashLoader size={50} color={"#123abc"} loading={isLoading} />
+                </div>
+            ) : (
+                renderStep()
+            )}
+            <div>
+                <CustomModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} />
+            </div>
         </div>
-      ) : (
-        renderStep()
-      )}
-      <div>
-        <CustomModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MultiStepForm;
