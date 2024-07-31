929b52575ce157375daf679d16f3523c4da1e32f
isakibul <isakibul108@gmail.com>
2024-07-31 09:17:03 +0600

    error text edited

--- src/form/ErrorPage.jsx
+++ src/form/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <h2>Oops! Something went wrong.</h2>
-            <p>An error occurred while processing your request. Please try again later.</p>
+            <p>We are sorry for the temporary failure. Please try again later.</p>
             <button style={{ width: '150px' }} onClick={() => navigate('/')}>Go Back</button>
         </div>
     );
